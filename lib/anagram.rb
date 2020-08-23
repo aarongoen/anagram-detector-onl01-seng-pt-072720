14424b52b16e0e29b5df9191b7e68c82a8bd6555
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-23 13:06:31 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,11 +11,11 @@ class Anagram
   def match(array_of_poss)
     mini_sorted_arrays = array_of_poss.each do |poss_anag|
       if poss_anag.split.sort == @word.split.sort 
-      @@matches << @word 
-        end
-      end 
+        @@matches << @word 
+      end
     end 
-  @@matches 
+    @@matches 
+  end 
   
 end 
 
