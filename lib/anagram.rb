b5282cde8426bbd47baea51108e64932e5faeb87
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-23 12:09:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+def Anagram
+attr_accessor :word 
+
+@@matches = []
+
+def initialize(word)
+  @word = word 
+end 
+
+def match[poss_anag]
+  poss_anag.each {|anag| anag == @word()
+  return all matches in that array 
+end 
+  
+end 
+
+#first get each 
